62ba839f70dd0abb1235e9200b7a9353193cd6b6
sunshiningsoo <yenkee1219@gmail.com>
2022-08-29 22:01:36 +0900

    N과 M 4: 백트래킹
    https://www.acmicpc.net/problem/15652

---
+++ python/BJ/bj15652.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+arr = []
+
+def backTracking(now):
+    if len(arr) == M:
+        for i in arr:
+            print(i, end=" ")
+        print("")
+    else:
+        for i in range(now, N+1):
+            arr.append(i)
+            backTracking(i)
+            arr.pop()
+
+backTracking(1)
