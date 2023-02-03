1ea9c507dbf051f9ffd831cf6487e2345a2a068a
sunshiningsoo <yenkee1219@gmail.com>
2023-02-03 10:52:44 +0900

    [범위 안에서 움직이기] lv1둘만의 암호-%의 사용
    https://school.programmers.co.kr/learn/courses/30/lessons/155652

--- python/Programmers/lv1둘만의암호.py
+++ python/Programmers/lv1둘만의암호.py
@@ -1,4 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/155652
+# 넘어가면 if문을 사용해 앞으로 옴겨주는 방식이 있지만,
+# 조금 더 응용해보면, 배열의 길이만큼을 나눈 나머지라고 생각해도 된다.
+
 def solution(s, skip, index):
     answer = ''
     alphabet = []
