9e23d8ac282ecdd5263ec1f9b1d5a9ae71e99c69
sunshiningsoo <yenkee1219@gmail.com>
2024-01-12 20:57:26 +0900

    [dfs 시간초과] 파이프 설치

---
+++ python/BJ/파이프옮기기_bj17070.py
@@ -0,0 +1,51 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+world = []
+for i in range(N):
+    world.append(list(map(int, input().split())))
+
+okpart1 = [[0, 1]] # 직선
+okpart2 = [[1, 0]] # 아래로
+okpart3 = [[0, 1], [1, 0], [1, 1]] # 대각선 아래로
+
+nxdir = [[0, 1], [1, 0], [1, 1]]
+
+obstacles = {0: okpart1, 1: okpart2, 2: okpart3}
+directions = [[0, 2], [1, 2], [0, 1, 2]]
+dir = 0
+
+curx, cury = 0, 1
+answer = 0
+
+def solution(x, y, curdir):
+    global answer
+    if [x, y] == [N-1, N-1]:
+        answer += 1
+        return
+
+    for nxtdir in directions[curdir]:
+        breakable = False
+        for xx, yy in obstacles[nxtdir]:
+            nx = xx + x
+            ny = yy + y
+            if nx < N and ny < N and world[nx][ny] == 0:
+                continue
+            else:
+                breakable = True
+                break
+
+        if not breakable:
+            solution(x + nxdir[nxtdir][0], y + nxdir[nxtdir][1], nxtdir)
+
+
+if world[-1][-1] == 1 or world[0][2] == 1:
+    print(answer)
+else:
+    solution(0, 1, 0)
+    print(answer)
+
+
+
+
