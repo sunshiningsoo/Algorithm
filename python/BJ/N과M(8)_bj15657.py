08807641bc807ff15354584891ebf98d1039cb9e
sunshiningsoo <yenkee1219@gmail.com>
2024-02-01 19:22:12 +0900

    [back] 조합, 중복 X

---
+++ python/BJ/N과M(8)_bj15657.py
@@ -0,0 +1,10 @@
+N, target = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+def back(cur, idx):
+    if len(cur) == target:
+        print(*cur)
+        return
+    for i in range(idx, len(nums)):
+        back(cur + [nums[i]], i)
+back([], 0)
