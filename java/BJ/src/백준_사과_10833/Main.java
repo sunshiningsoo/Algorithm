4fbf6d47d2f727f80cfa76276b673353a51943cf
sunshiningsoo <yenkee1219@gmail.com>
2024-02-16 14:53:32 +0900

    [imple] 단순 구현

---
+++ java/BJ/src/백준_사과_10833/Main.java
@@ -0,0 +1,23 @@
+package 백준_사과_10833;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for (int i=0;i<n;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            answer += b % a;
+        }
+
+        System.out.println(answer);
+    }
+}
