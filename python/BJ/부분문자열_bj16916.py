097b96c7b9497597e55e0f6a0d59567f49d28a0b
sunshiningsoo <yenkee1219@gmail.com>
2024-03-20 17:20:12 +0900

    [문자열] 이건좀..

---
+++ python/BJ/부분문자열_bj16916.py
@@ -0,0 +1,14 @@
+a = input()
+b = input()
+
+if b in a:
+    print(1)
+else:
+    print(0)
+
+# for i in range(len(a)-len(b)+1):
+#     if a[i] == b[0]:
+#         if a[i:i+len(b)] == b:
+#             ans = 1
+#             break
+
